7bdff3f9ddda2e61e80abdee61f520d854c0e5fd
Souljah Wagyu <ssmith274@student.rccd.edu>
2024-12-25 04:18:34 -0800

    test commit

---
+++ BankSim/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: souljah
+ *
+ * Created on December 25, 2024, 4:17 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
