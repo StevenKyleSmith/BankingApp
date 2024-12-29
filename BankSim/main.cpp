bd109440134786baf53da43c8806e7194a0a7030
Souljah Wagyu <ssmith274@student.rccd.edu>
2024-12-28 19:36:57 -0800

    added a load from file function, account creation format enhanced to include initial deposit, removed redundant code, improved deposit logic to prevent duplication and ensure proper account search, refactored saveToFile(),  and fixed formatting issue in file writing to avoid corrupt entries

--- BankSim/main.cpp
+++ BankSim/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
 using namespace std;
 void clearScreen();
 class BankAccount;
@@ -15,8 +16,10 @@ class System
 public:
     void createAccount(const string name, double initialBalance);
     BankAccount* findAccount(const string& accountNumber);
+    void loadFromFile(const string& fileName);
     void saveToFile(const string& fileName);
     vector<BankAccount> getAccounts() const;
+    bool findAcc(const string& account);
 private:
     vector<BankAccount> accounts;
 };
@@ -47,24 +50,200 @@ private:
     double balance;
     vector<Transaction> transactions;
 };
-
+void mainMenu();
 /*
  * 
  */
 int main(int argc, char** argv)
 {
     System bankingSystem;
+    BankAccount* verifyAccount;
+    vector<BankAccount> openAccounts;
+    bankingSystem.loadFromFile("accounts.txt");
     int choice;
-    string name;
     double initialDeposit;
+    double deposit, withdrawal;
+    string name;
     string option;
     string userAccNum;
-    BankAccount* verifyAccount;
-    double deposit, withdrawal;
-    vector<BankAccount> openAccounts;
     string case2Option, case3Option;
     do
     {
+        mainMenu();
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+                clearScreen();
+                cin.ignore();
+                cout << "    ==|Account Creation|==" << endl;
+                cout << "______________________________" << endl;
+                cout << "Please enter your first and last name: " << endl;
+                getline(cin, name);
+                cout << "Please enter your initial deposit amount: ";
+                cin >> initialDeposit;
+                while(initialDeposit < 0)
+                {
+                    cout << "Cannot deposit negative funds, please enter "
+                            "a positive amount: ";
+                    cin >> initialDeposit;
+                }
+                bankingSystem.createAccount(name, initialDeposit);
+                bankingSystem.saveToFile("accounts.txt");
+
+                cin.ignore();
+                break;
+            case 2:
+                clearScreen();
+                cin.ignore();
+                cout << "        ==|Deposits|==" << endl;
+                cout << "______________________________" << endl;
+                cout << "|LOGIN|" << endl;
+                cout << "Please enter your Account number: ";
+                cin >> userAccNum;
+                verifyAccount = bankingSystem.findAccount(userAccNum);
+                if(bankingSystem.findAcc(userAccNum))
+                {
+                    do
+                    {
+                        cout << "Please enter deposit amount: " << endl;
+                        cin >> deposit;
+                        while(deposit < 0)
+                        {
+                            cout << "Cannot deposit negative funds, please "
+                                    "enter a positive amount: ";
+                            cin >> deposit;
+                        }
+                        cout << endl << "Account Number: " << userAccNum << endl
+                                << "Amount to Deposit: " << fixed << 
+                                setprecision(2) << deposit << endl;
+
+                        if(verifyAccount != nullptr)
+                        {
+                            verifyAccount->deposit(deposit);
+                        }
+                        else
+                        {
+                            cout << "Account not found." << endl;
+                        }
+                        cout << "Deposit successful! " << endl;
+                        bankingSystem.saveToFile("accounts.txt");
+
+                        cout << "Would you like to make another deposit? (y/n)" 
+                                << endl;
+                        cin >> case2Option;
+                    }while(tolower(case2Option[0]) == 'y');
+                }
+                else
+                {
+                 cout << "Account Not Found. Please enter a valid account "
+                         "number in"
+                         " order to make deposits." << endl;   
+                }
+                break;
+            case 3:
+                clearScreen();
+                cin.ignore();
+                cout << "      ==|Withdrawals|==" << endl;
+                cout << "______________________________" << endl;
+                cout << "|LOGIN|" << endl;
+                cout << "Please enter your Account number: ";
+                cin >> userAccNum;
+                verifyAccount = bankingSystem.findAccount(userAccNum);
+                if(verifyAccount != nullptr)
+                {
+                    do
+                    {
+                        cout << "Please enter withdrawal amount: " << endl;
+                        cin >> withdrawal;
+                        while(withdrawal > verifyAccount->getBalance())
+                        {
+                            cout << "Insufficient funds, please enter a valid "
+                                    "amount: ";
+                            cin >> withdrawal;
+                        }
+                        cout << endl << "Account Number: " << userAccNum << endl
+                                << "Amount to Withdraw: " << fixed << 
+                                setprecision(2) << withdrawal << endl;
+
+                        verifyAccount->withdraw(withdrawal);
+                        cout << "withdraw successful! " << endl;
+                        bankingSystem.saveToFile("accounts.txt");
+
+                        cout << "Would you like to make another withdrawal? "
+                                "(y/n)" << endl;
+                        cin >> case3Option;
+                    }while(tolower(case3Option[0]) == 'y');
+                }
+                else
+                {
+                 cout << "Account Not Found. Please enter a valid account "
+                         "number in"
+                         " order to make deposits." << endl;   
+                }
+                break;
+            case 4:
+                clearScreen();
+                cin.ignore();
+                cout << "     ==|View Balance|==" << endl;
+                cout << "______________________________" << endl;
+                cout << "|LOGIN|" << endl;
+                cout << "Please enter your Account number: ";
+                cin >> userAccNum;
+                verifyAccount = bankingSystem.findAccount(userAccNum);
+                if(verifyAccount != nullptr)
+                {
+                    clearScreen();
+                    cout << "  Account Balance" << endl;
+                    cout << "____________________" << endl;
+                    cout << "Account Number: " << 
+                            verifyAccount->getAccountNumber() << endl;
+                    cout << "Active Checking: " << verifyAccount->getBalance();
+                    cout << endl << endl;
+                }
+                else
+                {
+                 cout << "Account Not Found. Please enter a valid account "
+                         "number in order to view account balances." << endl;   
+                }
+                break;
+            case 5:
+                clearScreen();
+                cin.ignore();
+                cout << "   ==|Transaction History|==" << endl;
+                cout << "______________________________" << endl;
+                cout << "|LOGIN|" << endl;
+                cout << "Please enter your Account number: ";
+                cin >> userAccNum;
+                verifyAccount = bankingSystem.findAccount(userAccNum);
+                if(verifyAccount != nullptr)
+                {
+                    clearScreen();
+                    cout << "Account Number: " << userAccNum << endl;
+                    verifyAccount->displayTransactions();
+                    cout << endl << endl;
+                }
+                else
+                {
+                 cout << "Account Not Found. Please enter a valid account "
+                         "number in order to view transaction history." << endl;   
+                }
+                break;
+            case 6:
+                return 1;
+                break;
+            default: 
+                cout << "Not a valid option." << endl;
+            }  
+            cout << "Return to main menu?(y/n)" << endl;
+            cin >> option;
+            clearScreen();
+        } while(tolower(option[0] == 'y'));
+    return 0;
+}
+void mainMenu()
+{
     cout << "Welcome to the Banking System Simulation" << endl;
     cout << "1. Create Account" << endl;
     cout << "2. Deposit" << endl;
@@ -73,162 +252,6 @@ int main(int argc, char** argv)
     cout << "5. Transaction History" << endl;
     cout << "6. Exit" << endl;
     cout << endl << "Enter your choice: ";
-    cin >> choice;
-    
-    switch(choice)
-    {
-        case 1:
-            clearScreen();
-            cin.ignore();
-            cout << "    ==|Account Creation|==" << endl;
-            cout << "______________________________" << endl;
-            cout << "Please enter your first and last name: " << endl;
-            getline(cin, name);
-            cout << "Please enter your initial deposit amount: ";
-            cin >> initialDeposit;
-            while(initialDeposit < 0)
-            {
-                cout << "Cannot deposit negative funds, please enter a positive amount: ";
-                cin >> initialDeposit;
-            }
-            bankingSystem.createAccount(name, initialDeposit);
-            bankingSystem.saveToFile("accounts.txt");
-            
-            cin.ignore();
-            break;
-        case 2:
-            clearScreen();
-            cin.ignore();
-            cout << "        ==|Deposits|==" << endl;
-            cout << "______________________________" << endl;
-            cout << "|LOGIN|" << endl;
-            cout << "Please enter your Account number: ";
-            cin >> userAccNum;
-            verifyAccount = bankingSystem.findAccount(userAccNum);
-            if(verifyAccount != nullptr)
-            {
-                do
-                {
-                    cout << "Please enter deposit amount: " << endl;
-                    cin >> deposit;
-                    while(deposit < 0)
-                    {
-                        cout << "Cannot deposit negative funds, please enter a positive amount: ";
-                        cin >> deposit;
-                    }
-                    cout << endl << "Account Number: " << userAccNum << endl
-                            << "Amount to Deposit: " << fixed << setprecision(2) 
-                            << deposit << endl;
-                    
-                    verifyAccount->deposit(deposit);
-                    cout << "Deposit successful! " << endl;
-                    bankingSystem.saveToFile("accounts.txt");
-                    
-                    cout << "Would you like to make another deposit? (y/n)" << endl;
-                    cin >> case2Option;
-                }while(tolower(case2Option[0]) == 'y');
-            }
-            else
-            {
-             cout << "Account Not Found. Please enter a valid account number in"
-                     " order to make deposits." << endl;   
-            }
-            break;
-        case 3:
-            clearScreen();
-            cin.ignore();
-            cout << "      ==|Withdrawals|==" << endl;
-            cout << "______________________________" << endl;
-            cout << "|LOGIN|" << endl;
-            cout << "Please enter your Account number: ";
-            cin >> userAccNum;
-            verifyAccount = bankingSystem.findAccount(userAccNum);
-            if(verifyAccount != nullptr)
-            {
-                do
-                {
-                    cout << "Please enter withdrawal amount: " << endl;
-                    cin >> withdrawal;
-                    while(withdrawal > verifyAccount->getBalance())
-                    {
-                        cout << "Insufficient funds, please enter a valid amount: ";
-                        cin >> withdrawal;
-                    }
-                    cout << endl << "Account Number: " << userAccNum << endl
-                            << "Amount to Withdraw: " << fixed << setprecision(2) 
-                            << withdrawal << endl;
-                    
-                    verifyAccount->withdraw(withdrawal);
-                    cout << "withdraw successful! " << endl;
-                    bankingSystem.saveToFile("accounts.txt");
-                    
-                    cout << "Would you like to make another withdrawal? (y/n)" << endl;
-                    cin >> case3Option;
-                }while(tolower(case3Option[0]) == 'y');
-            }
-            else
-            {
-             cout << "Account Not Found. Please enter a valid account number in"
-                     " order to make deposits." << endl;   
-            }
-            break;
-        case 4:
-            clearScreen();
-            cin.ignore();
-            cout << "     ==|View Balance|==" << endl;
-            cout << "______________________________" << endl;
-            cout << "|LOGIN|" << endl;
-            cout << "Please enter your Account number: ";
-            cin >> userAccNum;
-            verifyAccount = bankingSystem.findAccount(userAccNum);
-            if(verifyAccount != nullptr)
-            {
-                clearScreen();
-                cout << "  Account Balance" << endl;
-                cout << "____________________" << endl;
-                cout << "Account Number: " << verifyAccount->getAccountNumber()
-                        << endl;
-                cout << "Active Checking: " << verifyAccount->getBalance();
-                cout << endl << endl;
-            }
-            else
-            {
-             cout << "Account Not Found. Please enter a valid account number in"
-                     " order to view account balances." << endl;   
-            }
-            break;
-        case 5:
-            clearScreen();
-            cin.ignore();
-            cout << "   ==|Transaction History|==" << endl;
-            cout << "______________________________" << endl;
-            cout << "|LOGIN|" << endl;
-            cout << "Please enter your Account number: ";
-            cin >> userAccNum;
-            verifyAccount = bankingSystem.findAccount(userAccNum);
-            if(verifyAccount != nullptr)
-            {
-                clearScreen();
-                verifyAccount->displayTransactions();
-                cout << endl << endl;
-            }
-            else
-            {
-             cout << "Account Not Found. Please enter a valid account number in"
-                     " order to view transaction history." << endl;   
-            }
-            break;
-        case 6:
-            return 1;
-            break;
-        default: 
-            cout << "Not a valid option." << endl;
-        }  
-        cout << "Return to main menu?(y/n)" << endl;
-        cin >> option;
-        clearScreen();
-    } while(tolower(option[0] == 'y'));
-    return 0;
 }
 Transaction::Transaction()
 {
@@ -247,6 +270,52 @@ void BankAccount::addTransaction(const string& description, double amount)
 vector<BankAccount> System::getAccounts() const
 {
     return accounts;
+}
+bool System::findAcc(const string& accountNumber) 
+{
+    for (size_t i = 0; i < accounts.size(); ++i)
+    {
+        const BankAccount& account = accounts[i]; 
+        if (account.getAccountNumber() == accountNumber) 
+        {
+            return true; 
+        }
+    }
+    return false; 
+}
+void System::loadFromFile(const string& fileName)
+{
+    ifstream file(fileName);
+    if (file.is_open())
+    {
+        accounts.clear();
+        string accNumber, name;
+        double balance;
+        string line;
+        
+        while (getline(file, line))
+        {
+            stringstream ss(line);
+            getline(ss, accNumber, ',');
+            getline(ss, name, ',');
+            ss >> balance;
+            
+            if (!accNumber.empty() && !name.empty() && ss) 
+            {
+                accounts.emplace_back(accNumber, name, balance);
+            } 
+            else
+            {
+                cout << "Skipping invalid account entry in file: " << line << endl;
+            }
+        }
+        file.close();
+        cout << "Accounts loaded successfully from file." << endl;
+    }
+    else
+    {
+        cout << "Error: Could not open file for loading accounts." << endl;
+    }
 }
 void System::saveToFile(const string& fileName)
 {
@@ -282,7 +351,9 @@ void System::createAccount(const string name, double initialBalance)
 {
     string accountNumber = "ACC" + to_string(accounts.size() + 1);
     accounts.emplace_back(accountNumber, name, initialBalance);
-    cout << "Account successfully created! Account Number: " << accountNumber << endl;
+    cout << endl << "Account successfully created! Account Number: " 
+            << accountNumber << endl << "Name: " << name << endl << "Initial "
+            "Balance: " << initialBalance << endl;
 }
 void BankAccount::displayTransactions() const
 {
@@ -343,7 +414,6 @@ BankAccount::BankAccount()
     id = "0";
     name = "";
     balance = 0.0;
-    transactions.emplace_back();
 }
 void clearScreen()
 {
